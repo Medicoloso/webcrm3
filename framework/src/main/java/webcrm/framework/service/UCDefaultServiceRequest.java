48926580760a7a6c657cb84cd9f0889e1208f6a6
Nicola Hasler <nicola.hasler@gibmit.ch>
2018-06-15 14:58:24 +0200

    KNW 3

---
+++ framework/src/main/java/webcrm/framework/service/UCDefaultServiceRequest.java
@@ -0,0 +1,16 @@
+package webcrm.framework.service;
+
+/**
+ * This request object provides string based parameters. 
+ * 
+ * @author Christian Sauer (CSA), Giniality AG
+ */
+final class UCDefaultServiceRequest extends AbstractServiceRequest {
+
+	/**
+	 * Default constructor initializing mandatory fields.
+	 */
+	protected UCDefaultServiceRequest() {
+		super();
+	}
+}
